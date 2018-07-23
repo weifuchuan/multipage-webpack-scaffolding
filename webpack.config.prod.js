94fd757085fb62376104da372559dcdc01521d33
weifuchuan <fuchuan.wei@hotmail.com>
2018-07-23 13:27:01 +0800

    inline js css

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,33 +1,36 @@
 /* eslint-disable */
-const webpack = require('webpack');  
-const path = require('path')  
-const HtmlWebpackPlugin = require('html-webpack-plugin');  
-const ExtractTextPlugin = require("extract-text-webpack-plugin");  
+const webpack = require('webpack');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const pageConfig = require('./page.config.js');
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 
 class ChunksFromEntryPlugin {
-  apply (compiler) {
-      compiler.hooks.emit.tap('ChunksFromEntryPlugin', compilation => {
-          compilation.hooks.htmlWebpackPluginAlterChunks.tap(
-              'ChunksFromEntryPlugin',
-              (_, { plugin }) => {
-                  // takes entry name passed via HTMLWebpackPlugin's options
-                  const entry = plugin.options.entry;
-                  const entrypoint = compilation.entrypoints.get(entry);
+  apply(compiler) {
+    compiler.hooks.emit.tap('ChunksFromEntryPlugin', compilation => {
+      compilation.hooks.htmlWebpackPluginAlterChunks.tap(
+        'ChunksFromEntryPlugin',
+        (_, {
+          plugin
+        }) => {
+          // takes entry name passed via HTMLWebpackPlugin's options
+          const entry = plugin.options.entry;
+          const entrypoint = compilation.entrypoints.get(entry);
 
-                  return entrypoint.chunks.map(chunk =>
-                      ({
-                          names: chunk.name ? [chunk.name] : [],
-                          files: chunk.files.slice(),
-                          size: chunk.modulesSize(),
-                          hash: chunk.hash
-                      })
-                  );
-              }
+          return entrypoint.chunks.map(chunk =>
+            ({
+              names: chunk.name ? [chunk.name] : [],
+              files: chunk.files.slice(),
+              size: chunk.modulesSize(),
+              hash: chunk.hash
+            })
           );
-      });
+        }
+      );
+    });
   }
 }
 
@@ -38,13 +41,12 @@ let webpackConfig = {
   devtool: "source-map",
   // 配置出口  
   output: {
-    path: path.join(__dirname, "./dist/"),  
-    filename: 'static/js/[name].[hash:7].js',  
-    publicPath: '/',  
+    path: path.join(__dirname, "./dist/"),
+    filename: 'static/js/[name].[hash:7].js',
+    publicPath: '/',
   },
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.(js)$/,
         loader: 'eslint-loader',
         enforce: 'pre',
@@ -56,7 +58,7 @@ let webpackConfig = {
       // html中的img标签
       {
         test: /\.html$/,
-        loader:'html-withimg-loader',
+        loader: 'html-withimg-loader',
         include: [path.join(__dirname, "./src")],
         options: {
           limit: 10000,
@@ -98,7 +100,7 @@ let webpackConfig = {
           fallback: 'style-loader',
           use: ['css-loader'],
         }),
-      },      
+      },
       {
         test: /\.less$/,
         use: ExtractTextPlugin.extract({
@@ -108,26 +110,26 @@ let webpackConfig = {
       },
     ]
   },
-  plugins:[
+  plugins: [
     new UglifyJsPlugin({
-      sourceMap:true,
+      sourceMap: true,
       parallel: true
     }),
     new ExtractTextPlugin({
-     filename: 'static/css/[name].[hash:7].css'
+      filename: 'static/css/[name].[hash:7].css'
     }),
     //设置每一次build之前先删除dist  
-    new CleanWebpackPlugin(  
-      ['dist/*',],　     //匹配删除的文件  
-      {  
-          root: __dirname,   //根目录  
-          verbose: true,    //开启在控制台输出信息  
-          dry: false     //启用删除文件  
-      }  
+    new CleanWebpackPlugin(
+      ['dist/*', ], 　 //匹配删除的文件  
+      {
+        root: __dirname, //根目录  
+        verbose: true, //开启在控制台输出信息  
+        dry: false //启用删除文件  
+      }
     ),
     new ChunksFromEntryPlugin(),
   ],
-  optimization:{
+  optimization: {
     splitChunks: {
       chunks: "all",
       minSize: 30000,
@@ -137,44 +139,47 @@ let webpackConfig = {
       automaticNameDelimiter: '~',
       name: true,
       cacheGroups: {
-          vendors: {
-              test: /[\\/]node_modules[\\/]/,
-              priority: -10
-          },
-          // default: {
-          //     minChunks: 2,
-          //     priority: -20,
-          //     reuseExistingChunk: true
-          // },
-          commons: {
-            name: "commons",
-            chunks: "initial",
-            minChunks: 2
-          }
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        },
+        // default: {
+        //     minChunks: 2,
+        //     priority: -20,
+        //     reuseExistingChunk: true
+        // },
+        commons: {
+          name: "commons",
+          chunks: "initial",
+          minChunks: 2
+        }
       }
     }
   }
 };
 
-if(pageConfig && Array.isArray(pageConfig)){
+if (pageConfig && Array.isArray(pageConfig)) {
   pageConfig.map(page => {
     webpackConfig.entry[page.name] = `./src/pages/${page.jsEntry}`;
-    webpackConfig.plugins.push(new HtmlWebpackPlugin({
-      filename: path.join(__dirname,`/dist/${page.name}.html`),
-      template: path.join(__dirname,`/src/pages/${page.html}`),
-      inject: true,
-      entry: page.name,
-      chunks: [page.name],  
-      inlineSource: '.(js|css)$',
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeAttributeQuotes: true
-        // more options:
-        // https://github.com/kangax/html-minifier#options-quick-reference
-      },
-      chunksSortMode: 'dependency'
-    }))
+    webpackConfig.plugins.push(
+      new HtmlWebpackPlugin({
+        filename: path.join(__dirname, `/dist/${page.name}.html`),
+        template: path.join(__dirname, `/src/pages/${page.html}`),
+        inject: true,
+        entry: page.name,
+        chunks: [page.name],
+        inlineSource: '.(js|css)$',
+        minify: {
+          removeComments: true,
+          collapseWhitespace: true,
+          removeAttributeQuotes: true
+          // more options:
+          // https://github.com/kangax/html-minifier#options-quick-reference
+        },
+        chunksSortMode: 'dependency'
+      }),
+      new HtmlWebpackInlineSourcePlugin()
+    )
   })
 }
 
